399e3185752556ea202969016609eb17a2895b22
João Victor <52414666+joaovpimenta@users.noreply.github.com>
2022-03-29 08:40:15 -0300

    Início do Projeto

---
+++ src/main/java/com/cursospring/DemoMvcApplication.java
@@ -0,0 +1,18 @@
+package com.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Aplicação com Spring Web para treino.
+ * @author João Pimenta
+ * @since 29/03/2022
+ */
+@SpringBootApplication
+public class DemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+}
